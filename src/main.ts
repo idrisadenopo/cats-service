87b1639757b43e560b6c05f1a5ce6918179d82f0
Idris Adenopo <idris.adenopo@gmail.com>
2023-04-03 21:14:16 +0100

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = app.get(ConfigService);
   const secret = config.get('SESSION_SECRET');
-  app.setGlobalPrefix('api');
-  app.use(session({ secret, resave: false, saveUninitialized: true }));
+  // app.setGlobalPrefix('api');
+  app.use(
+    session({
+      secret,
+      resave: false,
+      saveUninitialized: true,
+    }),
+  );
   await app.listen(8080);
 }
 bootstrap();
