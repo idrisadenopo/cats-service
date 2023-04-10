e55aea4219b074c53ab806cc0cf2d7380accc816
Idris Adenopo <idris.adenopo@gmail.com>
2023-04-10 11:25:27 +0100

    test: fix broken tests

--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -3,9 +3,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { FavouritesService } from '../favourites/favourites.service';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { Request } from 'express';
 
 describe('CatsController', () => {
   let controller: CatsController;
+  const request = { session: { id: 'abc123' } };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,16 +20,16 @@ describe('CatsController', () => {
 
   describe('cats', () => {
     it('should return 5 cats', () => {
-      expect(controller.getFiveCats().cats.length).toBe(5);
+      expect(controller.getFiveCats(request as Request).cats.length).toBe(5);
     });
   });
 
   describe('cat', () => {
     it('should return 1 cat', () => {
-      const cat = controller.getCat();
+      const cat = controller.getCat(request as Request);
       expect(cat).toHaveProperty('id');
       expect(cat).toHaveProperty('name');
-      expect(cat).toHaveProperty('url');
+      expect(cat).toHaveProperty('imageData');
     });
   });
 
